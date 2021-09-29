3661d70d109a3e815fa44b463a694cf2075f4b9e
Alfredo <jarn.dev@gmail.com>
2021-09-29 00:36:32 -0300

    chore: update app module and main file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
+import { HeartbeatModule } from './heartbeat/heartbeat.module';
+import { env, postgresConstants } from './constants';
+import { Todo } from './todo/todo.entity';
+import { Auth } from './auth/auth.entity';
+import { TasksModule } from './tasks/tasks.module';
+import { Task } from './tasks/task.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { MulterModule } from '@nestjs/platform-express';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MulterModule.register({
+      dest: '../_upload',
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '_upload'),
+      serveRoot: '/images',
+      serveStaticOptions: {
+        dotfiles: 'deny',
+        index: false,
+        redirect: false,
+        extensions: ['jpg'],
+      },
+    }),
+    HeartbeatModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: postgresConstants.host,
+      port: parseInt(postgresConstants.port, 10),
+      username: postgresConstants.user,
+      password: postgresConstants.password,
+      database: postgresConstants.database,
+      models: [Auth, Todo, Task],
+    }),
+    TodoModule,
+    AuthModule,
+    TasksModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import swaggerAPI from './swagger/api';
+
+const logger = new Logger('bootstrap');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api', { exclude: ['heartbeat'] });
+  app.useGlobalPipes(new ValidationPipe());
+  swaggerAPI(app);
   await app.listen(3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
